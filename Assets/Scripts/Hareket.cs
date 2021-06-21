f8051345cd7dcf2ac4c590b83912daaa54c4888e
TarikCakmakci <tarikcakmakci@outlook.com>
2021-06-21 20:31:17 +0300

    Character Movement

---
+++ Assets/Scripts/Hareket.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public enum SIDE { Sol, Orta, Sag }
+
+public class Hareket : MonoBehaviour
+{
+
+    public SIDE m_Side = SIDE.Orta;
+    float YeniXPoz = 0f;
+    public bool SolaKaydir;
+    public bool SagaKaydir;
+    public float XDegeri;
+    private Animator m_Animator;
+    private CharacterController m_char;
+    public float GecisHizi;
+    private float x;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_char = GetComponent<CharacterController>();
+        transform.position = Vector3.zero;
+
+    }
+
+    void Update()
+    {
+        SolaKaydir = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow);
+        SagaKaydir = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow);
+        if (SolaKaydir)
+        {
+            if (m_Side == SIDE.Orta)
+            {
+                YeniXPoz = -XDegeri;
+                m_Side = SIDE.Sol;
+                m_Animator.Play("RightDodge");
+            } else if (m_Side == SIDE.Sag)
+            {
+                YeniXPoz = 0;
+                m_Side = SIDE.Orta;
+                m_Animator.Play("RightDodge");
+            }
+
+        }
+        else if (SagaKaydir)
+        {
+            if (m_Side == SIDE.Orta)
+            {
+                YeniXPoz = XDegeri;
+                m_Side = SIDE.Sag;
+                m_Animator.Play("LeftDodge");
+
+            }
+            else if (m_Side == SIDE.Sol)
+            {
+                YeniXPoz = 0;
+                m_Side = SIDE.Orta;
+                m_Animator.Play("LeftDodge");
+            }
+
+        }
+        x = Mathf.Lerp(x, YeniXPoz, Time.deltaTime * GecisHizi);
+        m_char.Move((x - transform.position.x) * Vector3.right);
+    }
+}
